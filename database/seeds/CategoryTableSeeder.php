0e0cf410b1bdee20c05eb2d52655042d4bfbe4bd
valelotararo@gmail.com <valerua8235>
2019-07-09 16:14:19 -0300

    mini details
    mini details

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class CategorySeeder extends Seeder
+class CategoryTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,9 +11,7 @@ class CategorySeeder extends Seeder
      */
     public function run()
 	{
-
-    
-		DB::table('categories')->insert([
+    	DB::table('categories')->insert([
 			'name' => 'Jabones',
 			'top' => true,
 			'parent' => 0,

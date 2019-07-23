836a2c8b3179201ff33c77823368b8122a17c803
valelotararo@gmail.com <valerua8235>
2019-07-23 17:46:26 -0300

    login Nacho
    login popup Nacho

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,9 +13,56 @@
                                             <i class="fas fa-user-plus fa-md icon"></i> 
                                     </a>  
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                        <a class="dropdown-item" href="{{ route('login') }}"><i class="navuser">{{ __('Login') }}</i></a> 
+                                            <a class="dropdown-item loginButton" href=""><i class="navuser">Login</i></a>
                                         <a class="dropdown-item" href="{{ route('register') }}"><i class="navuser">{{ __('Registrarse') }}</i></a>
                                     </div>
+                                    <form class="formLogin" accept-charset="UTF-8" role="form" action="{{ route('login') }}" method="post">
+                                            @csrf
+                                              <a class="formLogin-close"></a>
+                                              <div class="panel-heading">
+                                                <h2 class="regtitulo">Usuario y Contraseña</h2><br>
+                                              </div>
+                                              <div id="errorLogin"></div>
+                                              <div class="form-group">
+                                                <input class="form-control @error('email') is-invalid @enderror" placeholder="E-mail" name="email" type="text" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                                @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                              </div>
+                                              <div class="form-group">
+                                                <input class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" type="password" required autocomplete="current-password">
+                                                @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                              </div>
+                                              <div class="checkbox">
+                                                  <input name="remember" type="checkbox" id="remember" {{ old('remember') ? 'checked' : '' }} >
+                                                  <label class="form-check-label" for="remember">
+                                                        {{ __('Recordarme') }}
+                                                  </label>
+                                              </div>
+                                              <br>
+                                              <input class="btn regboton" type="submit" value="Ingresar">
+                                              <hr>
+                                              <div class="forgot text-center">
+                                                  @if (Route::has('password.request'))
+                                                  <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                  {{ __('Olvide mi contraseña') }}
+                                              </a>
+                                               @endif
+                                             </div>
+                                             <hr>
+                                               <div class="text-center">¿Acaso no estas registrado?<br>
+                                                 <a class="btn regboton" href="register">Registrate</a>
+                                               </div>
+                                           </form>
+                          
+
+
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{route('carritoGuest')}}">
